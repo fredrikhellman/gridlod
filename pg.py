6293ac4b1a3c38da4892d436cb3e1c832c26cc00
Fredrik Hellman <fredrik.hellman@it.uu.se>
2017-02-01 09:55:45 +0100

    Added age and epsilon lists, and caches

--- pg.py
+++ pg.py
@@ -9,12 +9,18 @@ class PetrovGalerkinLOD:
     def __init__(self, world, k, IPatchGenerator, epsilonTol, printLevel=0):
         self.world = world
         NtCoarse = np.prod(world.NWorldCoarse)
-        self.ecList = [None]*NtCoarse
         self.k = k
         self.IPatchGenerator = IPatchGenerator
         self.epsilonTol = epsilonTol
         self.printLevel = printLevel
 
+        self.epsilonList = None
+        self.ageList = None
+        self.ecList = None
+        self.Kms = None
+        self.K = None
+        self.basisCorrectors = None
+        
     def updateCorrectors(self, coefficient, clearFineQuantities=True):
         world = self.world
         k = self.k
@@ -24,11 +30,29 @@ class PetrovGalerkinLOD:
         NtCoarse = np.prod(world.NWorldCoarse)
 
         saddleSolver = lod.schurComplementSolver(world.NWorldCoarse*world.NCoarseElement)
-        
+
+        # Reset all caches
+        self.Kms = None
+        self.K = None
+        self.basisCorrectors = None
+
+        if self.ecList is None:
+            self.ecList = [None]*NtCoarse
+
+        if self.ageList is None:
+            self.ageList = [-1]*NtCoarse
+
+        if self.epsilonList is None:
+            self.epsilonList = [np.nan]*NtCoarse
+
         ecList = self.ecList
+        ageList = self.ageList
+        epsilonList = self.epsilonList
         for TInd in range(NtCoarse):
             if self.printLevel > 0:
                 print str(TInd) + ' / ' + str(NtCoarse),
+
+            ageList[TInd] += 1
             iElement = util.convertpIndexToCoordinate(world.NWorldCoarse-1, TInd)
             if ecList[TInd] is not None  and  hasattr(coefficient, 'rCoarse'):
                 ecT = ecList[TInd]
@@ -37,11 +61,15 @@ class PetrovGalerkinLOD:
             else:
                 coefficientPatch = None
                 epsilonT = np.inf
+
+            epsilonList[TInd] = epsilonT
             
             if self.printLevel > 0:
-                print 'epsilonT = ' + str(epsilonT)
+                print 'epsilonT = ' + str(epsilonT), 
                 
             if epsilonT > epsilonTol:
+                if self.printLevel > 0:
+                    print 'C'
                 ecT = lod.elementCorrector(world, k, iElement, saddleSolver)
 
                 if coefficientPatch is None:
@@ -53,12 +81,21 @@ class PetrovGalerkinLOD:
                 if clearFineQuantities:
                     ecT.clearFineQuantities()
                 ecList[TInd] = ecT
-                
+                ageList[TInd] = 0
+            else:
+                if self.printLevel > 0:
+                    print 'N'
+
     def clearCorrectors(self):
         NtCoarse = np.prod(self.world.NWorldCoarse)
-        self.ecList = [None]*NtCoarse
+        self.ecList = None
 
     def assembleBasisCorrectors(self):
+        if self.basisCorrectors is not None:
+            return self.basisCorrectors
+
+        assert(self.ecList is not None)
+
         world = self.world
         NWorldCoarse = world.NWorldCoarse
         NCoarseElement = world.NCoarseElement
@@ -95,10 +132,16 @@ class PetrovGalerkinLOD:
             data.extend(dataT)
 
         basisCorrectors = sparse.csc_matrix((data, (rows, cols)), shape=(NpFine, NpCoarse))
-        
+
+        self.basisCorrectors = basisCorrectors
         return basisCorrectors
         
     def assembleMsStiffnessMatrix(self):
+        if self.Kms is not None:
+            return self.Kms
+
+        assert(self.ecList is not None)
+
         world = self.world
         NWorldCoarse = world.NWorldCoarse
         
@@ -130,10 +173,16 @@ class PetrovGalerkinLOD:
             data.extend(dataT)
 
         Kms = sparse.csc_matrix((data, (rows, cols)), shape=(NpCoarse, NpCoarse))
-        
+
+        self.Kms = Kms
         return Kms
 
     def assembleStiffnessMatrix(self):
+        if self.K is not None:
+            return self.K
+
+        assert(self.ecList is not None)
+
         world = self.world
         NWorldCoarse = world.NWorldCoarse
         
@@ -162,6 +211,7 @@ class PetrovGalerkinLOD:
             data.extend(dataT)
 
         K = sparse.csc_matrix((data, (rows, cols)), shape=(NpCoarse, NpCoarse))
-        
+
+        self.K = K
         return K
     
