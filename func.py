594892841a0e1a634e31d6a62d05b17c26311db6
Fredrik Hellman <fredrik.hellman@gmail.com>
2018-09-19 20:46:50 +0200

    Add function to compute derivative of CQ1 functions.

--- func.py
+++ func.py
@@ -1,44 +1,10 @@
 import util
 import numpy as np
-    
-def evaluateP0(N, p0, coordinates):
-    """Compute a piecewise constant (p0) function on an N-grid at the
-       given coordinates. The function can be tensor-valued.
-    """
 
+def _computeCoordinateIndexParts(N, coordinates):
     assert(np.all(coordinates <= 1.0) and np.all(coordinates >= 0.0))
-    assert(p0.shape[0] == np.prod(N))
     
     eps = np.finfo(np.float64).eps
-
-    # Find what index coordinates the coordinates are in
-    coordIndicesDecimal = coordinates.astype(np.float64)*N
-    coordIndicesDecimal *= (1.-eps) # This guarantees we can call with coordinate 1.0
-    coordIndices = np.floor(coordIndicesDecimal).astype(np.int64)
-
-    # Convert to linear indices
-    linearIndices = util.convertpCoordIndexToLinearIndex(N-1, coordIndices)
-
-    # Lookup function values
-    evaluatedP0 = p0[linearIndices,...]
-    
-    return evaluatedP0
-
-def evaluateP1(N, p1, coordinates):
-    """Compute a piecewise constant (p1) function on an N-grid at the
-       given coordinates. The function can be tensor-valued.
-
-    d-linear interpolation is used, i.e. linear in 1d, bilinear in 2d
-    etc.
-
-    """
-
-    assert(np.all(coordinates <= 1.0) and np.all(coordinates >= 0.0))
-    assert(p1.shape[0] == np.prod(N+1))
-    
-    eps = np.finfo(np.float64).eps
-
-    d = N.size
     
     # Find what index coordinates the coordinates are in
     coordIndicesDecimal = coordinates.astype(np.float64)*N
@@ -48,6 +14,43 @@ def evaluateP1(N, p1, coordinates):
     # The fractional part gives the interpolation weights within the element
     coordIndicesIpart = np.floor(coordIndicesDecimal).astype(np.int64)
     coordIndicesFpart = coordIndicesDecimal - coordIndicesIpart
+
+    return coordIndicesIpart, coordIndicesFpart
+
+def evaluateDQ0(N, dq0, coordinates):
+    """Compute a discontinuous piecewise constant (DQ0) function on an
+       N-grid at the given coordinates. The function can be
+       tensor-valued.
+
+    """
+    assert(coordinates.ndim == 2)
+    assert(dq0.shape[0] == np.prod(N))
+    
+    coordIndices, _ = _computeCoordinateIndexParts(N, coordinates)
+    
+    # Convert to linear indices
+    linearIndices = util.convertpCoordIndexToLinearIndex(N-1, coordIndices)
+
+    # Lookup function values
+    evaluatedDQ0 = dq0[linearIndices,...]
+    
+    return evaluatedDQ0
+
+def evaluateCQ1(N, cq1, coordinates):
+    """Compute a continuous piecewise d-linear (CQ1) function on an N-grid
+       at the given coordinates. The function can be tensor-valued.
+
+    d-linear interpolation is used, i.e. linear in 1d, bilinear in 2d
+    etc.
+
+    """
+
+    assert(coordinates.ndim == 2)
+    assert(cq1.shape[0] == np.prod(N+1))
+
+    d = N.size
+    
+    coordIndicesIpart, coordIndicesFpart = _computeCoordinateIndexParts(N, coordinates)
 
     # Compute interpolation weights
     interpolationWeights = np.zeros((coordinates.shape[0], 2**d))
@@ -59,7 +62,9 @@ def evaluateP1(N, p1, coordinates):
         # localNode for all points. It is a product of either x_k or
         # 1-x_k for each dimension k, depending on the node. The
         # bitmap tells us which.
-       
+
+        print localNodeBitmap.shape
+        print coordIndicesFpart.shape
         interpolationWeights[:, localNode] = np.prod(
             (1.0 - localNodeBitmap) + (2.0*localNodeBitmap - 1.0)*coordIndicesFpart,
             axis=1)
@@ -69,9 +74,53 @@ def evaluateP1(N, p1, coordinates):
     interpolationValuesIndices = np.add.outer(coordIndicesLinear, util.elementpIndexMap(N))
 
     # Compute the values of the relevant nodes
-    interpolationValues = p1[interpolationValuesIndices,...]
+    interpolationValues = cq1[interpolationValuesIndices,...]
 
     # Perform the interpolation sum of weight-value products
-    evaluatedP1 = np.einsum('pi...,pi...->p...', interpolationWeights, interpolationValues)
+    evaluatedCQ1 = np.einsum('pi,pi...->p...', interpolationWeights, interpolationValues)
     
-    return evaluatedP1
+    return evaluatedCQ1
+
+def evaluateCQ1D(N, cq1, coordinates):
+    """Compute the derivative of a continuous piecewise d-linear (cq1)
+       function on an N-grid at the given coordinates. The function
+       can be tensor-valued.
+
+    """
+
+    assert(coordinates.ndim == 2)
+    assert(cq1.shape[0] == np.prod(N+1))
+
+    d = N.size
+    
+    coordIndicesIpart, coordIndicesFpart = _computeCoordinateIndexParts(N, coordinates)
+
+    # Compute interpolation weights
+    interpolationWeights = np.zeros((coordinates.shape[0], 2**d, d))
+    for localNode in range(2**d):
+        localNodeBinaryString = bin(localNode)[2:].zfill(d)[::-1]
+        localNodeBitmap = np.array([0 if bit is '0' else 1 for bit in localNodeBinaryString])
+        for dimensionToDifferentiate in range(d):
+            # These are the factors x_k or (1-x_k)
+            interpolationWeightsFactors = (1.0 - localNodeBitmap) + (2.0*localNodeBitmap - 1.0)*coordIndicesFpart
+
+            # Chain rule: replace the factor that depends on the dimension we differentiate with the derivative
+            interpolationWeightsFactors[..., dimensionToDifferentiate] = N[dimensionToDifferentiate]*(2.0*localNodeBitmap[dimensionToDifferentiate] - 1.0)
+
+            # Multiply them
+            interpolationWeightsForThisDimension = np.prod(interpolationWeightsFactors, axis=1)
+
+            # Insert into interpolation weight matrix
+            interpolationWeights[:, localNode, dimensionToDifferentiate] = interpolationWeightsForThisDimension
+
+    # Compute the global indices of all the local nodes
+    coordIndicesLinear = util.convertpCoordIndexToLinearIndex(N, coordIndicesIpart)
+    interpolationValuesIndices = np.add.outer(coordIndicesLinear, util.elementpIndexMap(N))
+
+    # Compute the values of the relevant nodes
+    interpolationValues = cq1[interpolationValuesIndices,...]
+
+    # Perform the interpolation sum of weight-value products
+    evaluatedCQ1 = np.einsum('pil,pi...->p...l', interpolationWeights, interpolationValues)
+    
+    return evaluatedCQ1
--- test/test_func.py
+++ test/test_func.py
@@ -4,9 +4,9 @@ from gridlod import func, util
 import numpy as np
 
 class evaluate_TestCase(unittest.TestCase):
-    def test_evaluateP0(self):
+    def test_evaluateDQ0(self):
         N = np.array([3, 2])
-        p0 = np.array([10, 20, 30, 40, 50, 60])
+        dq0 = np.array([10, 20, 30, 40, 50, 60])
 
         x = np.array([[0.1, 0.1],
                       [0.9, 0.1],
@@ -14,19 +14,19 @@ class evaluate_TestCase(unittest.TestCase):
                       [0.0, 0.0],
                       [1.0, 1.0]])
 
-        p0OfX = func.evaluateP0(N, p0, x)
-        p0ShouldBe = np.array([10, 30, 50, 10, 60])
+        dq0OfX = func.evaluateDQ0(N, dq0, x)
+        dq0ShouldBe = np.array([10, 30, 50, 10, 60])
 
-        self.assertTrue(np.allclose(p0OfX, p0ShouldBe))
+        self.assertTrue(np.allclose(dq0OfX, dq0ShouldBe))
 
-    def test_evaluateP0TensorValued(self):
+    def test_evaluateDQ0TensorValued(self):
         N = np.array([3, 2])
-        p0 = np.array([[[10, 11]],
-                       [[20, 21]],
-                       [[30, 31]],
-                       [[40, 41]],
-                       [[50, 51]],
-                       [[60, 61]]])
+        dq0 = np.array([[[10, 11]],
+                        [[20, 21]],
+                        [[30, 31]],
+                        [[40, 41]],
+                        [[50, 51]],
+                        [[60, 61]]])
 
         x = np.array([[0.1, 0.1],
                       [0.9, 0.1],
@@ -34,18 +34,33 @@ class evaluate_TestCase(unittest.TestCase):
                       [0.0, 0.0],
                       [1.0, 1.0]])
 
-        p0OfX = func.evaluateP0(N, p0, x)
-        p0ShouldBe = np.array([[[10, 11]],
-                               [[30, 31]],
-                               [[50, 51]],
-                               [[10, 11]],
-                               [[60, 61]]])
+        dq0OfX = func.evaluateDQ0(N, dq0, x)
+        dq0ShouldBe = np.array([[[10, 11]],
+                                [[30, 31]],
+                                [[50, 51]],
+                                [[10, 11]],
+                                [[60, 61]]])
 
-        self.assertTrue(np.allclose(p0OfX, p0ShouldBe))
+        self.assertTrue(np.allclose(dq0OfX, dq0ShouldBe))
 
-    def test_evaluateP1(self):
+    def test_evaluateCQ1_1d(self):
+        N = np.array([3])
+        cq1 = np.array([10, 20, 25, 0])
+
+        x = np.array([[0],
+                      [1./6],
+                      [0.5],
+                      [2./3],
+                      [1.0]])
+
+        cq1OfX = func.evaluateCQ1(N, cq1, x)
+        cq1ShouldBe = np.array([10, 15, 22.5, 25, 0])
+
+        self.assertTrue(np.allclose(cq1OfX, cq1ShouldBe))
+
+    def test_evaluateCQ1_2d(self):
         N = np.array([3, 2])
-        p1 = np.array([10, 10, 30, 30,
+        cq1 = np.array([10, 10, 30, 30,
                        60, 60, 80, 80,
                        60, 60, 90, 100])
 
@@ -57,8 +72,61 @@ class evaluate_TestCase(unittest.TestCase):
                       [5./6, 1.0],
                       [5./6,  .75]])
 
-        p1OfX = func.evaluateP1(N, p1, x)
-        p1ShouldBe = np.array([20, 40, 70, 10, 100, 95, 87.5])
+        cq1OfX = func.evaluateCQ1(N, cq1, x)
+        cq1ShouldBe = np.array([20, 40, 70, 10, 100, 95, 87.5])
 
-        self.assertTrue(np.allclose(p1OfX, p1ShouldBe))
+        self.assertTrue(np.allclose(cq1OfX, cq1ShouldBe))
         
+
+    def test_evaluateCQ1D_1d(self):
+        N = np.array([3])
+        cq1 = np.array([0.0, 1.0, 3.0, 103.0])
+
+        x = np.array([[0.0],
+                      [0.5],
+                      [0.55],
+                      [0.99]])
+
+        cq1dOfX = func.evaluateCQ1D(N, cq1, x)
+        print cq1dOfX
+        cq1dShouldBe = np.array([[3.0], [6.0], [6.0], [300.0]])
+
+        self.assertTrue(np.allclose(cq1dOfX, cq1dShouldBe))
+
+    def test_evaluateCQ1D_2d(self):
+        N = np.array([3, 2])
+        cq1 = np.array([10, 10, 30, 30,
+                        60, 60, 80, 80,
+                        60, 60, 90, 100])
+
+        x = np.array([[0.1, 0.1],
+                      [0.5, 0.0],
+                      [0.5, 0.75],
+                      [0.1, 0.6]])
+
+        cq1dOfX = func.evaluateCQ1D(N, cq1, x)
+        cq1dShouldBe = np.array([[0.0,  100.0],
+                                 [60.0, 100.0],
+                                 [75.0,  10.0],
+                                 [0.0,    0.0]])
+
+        self.assertTrue(np.allclose(cq1dOfX, cq1dShouldBe))
+
+    def test_evaluateCQ1D_2d_tensorValued(self):
+        N = np.array([3, 2])
+        cq1 = np.array([[10, 100], [10, 100], [30, 300], [30, 300],
+                        [60, 600], [60, 600], [80, 800], [80, 800],
+                        [60, 600], [60, 600], [90, 900], [100, 1000]])
+
+        x = np.array([[0.1, 0.1],
+                      [0.5, 0.0],
+                      [0.5, 0.75],
+                      [0.1, 0.6]])
+
+        cq1dOfX = func.evaluateCQ1D(N, cq1, x)
+        cq1dShouldBe = np.array([[[0.0,  100.0], [0,     1000.0]],
+                                 [[60.0, 100.0], [600.0, 1000.0]],
+                                 [[75.0,  10.0], [750.0,  100.0]],
+                                 [[0.0,    0.0], [00.0,     0.0]]])
+
+        self.assertTrue(np.allclose(cq1dOfX, cq1dShouldBe))
