86a339fe0ead36ff5c91013feb1ffe056757127d
Fredrik Hellman <fredrik.hellman@gmail.com>
2019-01-25 15:40:13 +0100

    Create gridlod distribution with two packages.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='gridlod',
+      version='2019.1.25',
+      description='LOD on d-dimensional grids',
+      url='http://github.com/fredrikhellman/gridlod',
+      author='Fredrik Hellman',
+      author_email='fredrik.hellman@gmail.com',
+      license='MIT',
+      packages=['gridlod', 'gridlod-ipp'],
+      zip_safe=False)
