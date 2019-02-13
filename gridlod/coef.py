bc5c9ec7cf3ced229d8cda3e9cdef7e4ff894a93
Fredrik Hellman <fredrik.hellman@gmail.com>
2019-02-13 09:09:34 +0100

    Add simple localize coefficient function. The others are to be removed.

--- gridlod/coef.py
+++ gridlod/coef.py
@@ -2,6 +2,22 @@ import numpy as np
 
 from . import util
 
+def localizeCoefficient(patch, aFine):
+    iPatchWorldCoarse = patch.iPatchWorldCoarse
+    NPatchCoarse = patch.NPatchCoarse
+    NCoarseElement = patch.world.NCoarseElement
+    NPatchFine = NPatchCoarse*NCoarseElement
+    iPatchWorldFine = iPatchWorldCoarse*NCoarseElement
+    NWorldFine = patch.world.NWorldFine
+    
+    # a
+    coarsetIndexMap = util.lowerLeftpIndexMap(NPatchFine-1, NWorldFine-1)
+    coarsetStartIndex = util.convertpCoordIndexToLinearIndex(NPatchFine-1, iPatchWorldFine)
+    aFineLocalized = aFine[coarsetStartIndex + coarsetIndexMap]
+    
+    return aFineLocalized
+
+
 class CoefficientAbstract:
     @property
     def aFine(self):
--- gridlod/test/test_coef.py
+++ gridlod/test/test_coef.py
@@ -2,6 +2,20 @@ import unittest
 import numpy as np
 
 from gridlod import coef
+from gridlod.world import World, Patch
+
+class localizeCoefficient_TestCase(unittest.TestCase):
+    def test_localizeCoefficient(self):
+        NWorldCoarse = np.array([2, 3])
+        NCoarseElement = np.array([2, 1])
+        world = World(NWorldCoarse, NCoarseElement)
+        patch = Patch(world, 1, 0)
+        aFine = np.array([1, 2, 3, 4,
+                          10, 20, 30, 40,
+                          100, 200, 300, 400])
+        aFineLocalized = coef.localizeCoefficient(patch, aFine)
+        print(aFineLocalized)
+        self.assertTrue(np.all(aFineLocalized == aFine[:8]))
 
 class coefficientCoarseFactor_TestCase(unittest.TestCase):
     def test_coefficientCoarseFactor(self):
