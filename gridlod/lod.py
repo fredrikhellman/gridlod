dd7e525d8e393f8a2d07cdbab8cd748d860913b8
Fredrik Hellman <fredrik.hellman@gmail.com>
2019-02-28 13:43:59 +0100

    Add a way to compute error indicators given greeks. The greeks have to be computed elsewhere, depending on application.

--- gridlod/lod.py
+++ gridlod/lod.py
@@ -28,7 +28,7 @@ class NullspaceOneLevelHierarchySolver:
         # It is time consuming to create PPatch. Perhaps another
         # solver of this kind can be implemented, but where PPatch is
         # implicitly defined instead...
-        
+
     def solve(self, A, I, bList, fixed, NPatchCoarse=None, NCoarseElement=None):
         return linalg.saddleNullSpaceHierarchicalBasis(A, I, self.PPatch, bList, self.coarseNodes, fixed)
 
@@ -45,7 +45,7 @@ class NullspaceSeveralLevelsHierarchySolver:
 class BlockDiagonalPreconditionerSolver:
     def __init__(self):
         pass
-    
+
     def solve(self, A, I, bList, fixed, NPatchCoarse=None, NCoarseElement=None):
         raise NotImplementedError
         return linalg.solveWithBlockDiagonalPreconditioner(A, I, bList)
@@ -56,17 +56,17 @@ class SchurComplementSolver:
             self.cholCache = linalg.choleskyCache(NCache)
         else:
             self.cholCache = None
-    
+
     def solve(self, A, I, bList, fixed, NPatchCoarse=None, NCoarseElement=None):
         return linalg.schurComplementSolve(A, I, bList, fixed, NPatchCoarse, NCoarseElement, self.cholCache)
 
 class DirectSolver:
     def __init__(self):
         pass
-    
+
     def solve(self, A, I, bList, fixed, NPatchCoarse=None, NCoarseElement=None):
         return linalg.saddleDirect(A, I, bList, fixed)
-    
+
 def ritzProjectionToFinePatch(patch,
                               APatchFull,
                               bPatchFullList,
@@ -87,7 +87,7 @@ def ritzProjectionToFinePatch(patch,
 
     inherit0 = patch.iPatchWorldCoarse==0
     inherit1 = (patch.iPatchWorldCoarse + patch.NPatchCoarse)==world.NWorldCoarse
-    
+
     boundaryMap = np.ones([d, 2], dtype='bool')
     boundaryMap[inherit0,0] = boundaryMapWorld[inherit0,0]
     boundaryMap[inherit1,1] = boundaryMapWorld[inherit1,1]
@@ -95,9 +95,9 @@ def ritzProjectionToFinePatch(patch,
     # Using schur complement solver for the case when there are no
     # Dirichlet conditions does not work. Fix if necessary.
     assert(np.any(boundaryMap == True))
-    
+
     fixed = util.boundarypIndexMap(NPatchFine, boundaryMap)
-    
+
     #projectionsList = saddleSolver.solve(APatch, IPatch, bPatchList)
 
     projectionsList = saddleSolver.solve(APatchFull, IPatch, bPatchFullList, fixed, patch.NPatchCoarse, world.NCoarseElement)
@@ -114,7 +114,7 @@ class CoarseScaleInformationFlux:
     def __init__(self, correctorFluxTF, basisFluxTF):
         self.correctorFluxTF = correctorFluxTF
         self.basisFluxTF = basisFluxTF
-        
+
 def computeElementCorrector(patch, IPatch, aPatch, ARhsList=None, MRhsList=None, saddleSolver=None):
     '''Compute the fine correctors over a patch.
 
@@ -125,11 +125,11 @@ def computeElementCorrector(patch, IPatch, aPatch, ARhsList=None, MRhsList=None,
 
     while callable(IPatch):
         IPatch = IPatch()
-        
+
     while callable(aPatch):
         aPatch = aPatch()
-    
-    
+
+
     assert(ARhsList is not None or MRhsList is not None)
     numRhs = None
 
@@ -208,16 +208,16 @@ def computeBasisCorrectors(patch, IPatch, aPatch, saddleSolver=None):
 
     while callable(aPatch):
         aPatch = aPatch()
-    
+
     d = np.size(patch.NPatchCoarse)
     ARhsList = list(map(np.squeeze, np.hsplit(patch.world.localBasis, 2**d)))
 
     return computeElementCorrector(patch, IPatch, aPatch, ARhsList, saddleSolver=None)
 
-    
+
 def computeErrorIndicatorFine(patch, correctorsList, aPatchOld, aPatchNew):
-    ''' Compute the fine error idicator e(T). 
-    
+    ''' Compute the fine error idicator e(T).
+
     This requires all correctors and the new and old coefficient.
     '''
 
@@ -275,9 +275,36 @@ def computeErrorIndicatorFine(patch, correctorsList, aPatchOld, aPatchNew):
 
     return np.sqrt(epsilonTSquare)
 
-def computeErrorIndicatorCoarseExact(patch, muTPrime, aPatchOld, aPatchNew):
+def computeErrorIndicatorCoarseFromGreeks(patch, muTPrime, greeksPatch):
+    ''' Compute the coarse error idicator E(T) from the "greeks" kappa and delta,
+    where
+
+    kappaMaxT = || AOld ANew^{-1} ||_max(T)
+                                 over the patch coarse T (patch.iPatchWorldCoarse)
+
+    deltaMaxTPrime = || ANew^{-1/2} (ANew - AOld) AOld^{1/2} ||_max(TPrime)
+                                                     over all coarse elements TPrime in the patch
+
+    This requires muTPrime from CSI and the new and old coefficient.
+
+    greeksPatch should either be
+      1. callable and returns (kappaMaxT, deltaMaxTPrime)
+      2. the tuple (kappaMaxT, deltaMaxTPrime)
+    '''
+
+    while callable(greeksPatch):
+        greeksPatch = greeksPatch()
+
+    deltaMaxTPrime, kappaMaxT = greeksPatch
+
+    epsilonTSquare = kappaMaxT * \
+                     np.sum((deltaMaxNormTPrime**2)*muTPrime)
+
+    return np.sqrt(epsilonTSquare)
+
+def computeErrorIndicatorCoarseFromCoefficients(patch, muTPrime, aPatchOld, aPatchNew):
     ''' Compute the coarse error idicator E(T) with explicit value of AOld and ANew.
-    
+
     This requires muTPrime from CSI and the new and old coefficient.
     '''
 
@@ -289,10 +316,10 @@ def computeErrorIndicatorCoarseExact(patch, muTPrime, aPatchOld, aPatchNew):
 
     while callable(aPatchNew):
         aPatchNew = aPatchNew()
-        
+
     aTilde = aPatchOld
     a = aPatchNew
-    
+
     assert(a.ndim == 1) # Matrix-valued A not supported in thus function yet
 
     world = patch.world
@@ -313,10 +340,7 @@ def computeErrorIndicatorCoarseExact(patch, muTPrime, aPatchOld, aPatchNew):
     deltaMaxNormTPrime = np.max(np.abs((aTPrime - aTildeTPrime)/np.sqrt(aTPrime*aTildeTPrime)), axis=1)
     kappaMaxT = np.max(np.abs(aTPrime[elementCoarseIndex]/aTildeTPrime[elementCoarseIndex]))
 
-    epsilonTSquare = kappaMaxT * \
-                     np.sum((deltaMaxNormTPrime**2)*muTPrime)
-
-    return np.sqrt(epsilonTSquare)
+    return computeErrorIndicatorCoarseFromGreeks(patch, muTPrime, (kappaMaxT, deltaMaxNormTPrime))
 
 def computeCoarseQuantities(patch, correctorsList, aPatch):
     '''Compute the coarse quantities K and L for this element corrector
@@ -334,10 +358,10 @@ def computeCoarseQuantities(patch, correctorsList, aPatch):
 
     LTT'ij = (A \nabla (chi_T - Q_T)lambda_j, \nabla (chi_T - Q_T) lambda_j)_{T'}
     '''
-    
+
     while callable(aPatch):
         aPatch = aPatch()
-        
+
     world = patch.world
     NCoarseElement = world.NCoarseElement
     NPatchCoarse = patch.NPatchCoarse
@@ -373,9 +397,9 @@ def computeCoarseQuantities(patch, correctorsList, aPatch):
     # This loop can probably be done faster than this. If a bottle-neck, fix!
     Kmsij = np.zeros((NpPatchCoarse, 2**d))
     LTPrimeij = np.zeros((NTPrime, 2**d, 2**d))
-    for (TPrimeInd, 
-         TPrimeCoarsepStartIndex, 
-         TPrimeFinetStartIndex, 
+    for (TPrimeInd,
+         TPrimeCoarsepStartIndex,
+         TPrimeFinetStartIndex,
          TPrimeFinepStartIndex) \
          in zip(np.arange(NTPrime),
                 TPrimeCoarsepStartIndices,
@@ -409,65 +433,11 @@ def computeCoarseQuantities(patch, correctorsList, aPatch):
 
     return CoarseScaleInformation(Kij, Kmsij, muTPrime)
 
-def computeErrorIndicatorCoarse(self, delta):
-    assert(self.csi is not None)
-
-    world = self.world
-    NPatchCoarse = world.NWorldCoarse
-    NCoarseElement = world.NCoarseElement
-
-    # for localize   #NAMECHANGING horribly #k included here
-    iElementPatchCoarse = self.iElementPatchCoarse
-    NSubPatchCoarse = self.NPatchCoarse
-    iSubPatchCoarse = self.iPatchWorldCoarse
-
-    # gibt den index in coarseTIndexMap an, der fuer das element steht
-    elementCoarseIndex = util.convertpCoordIndexToLinearIndex(NPatchCoarse - 1, iElementPatchCoarse)
-
-    # gibt die gestalt des patches an
-    coarseTIndexMap = util.lowerLeftpIndexMap(NSubPatchCoarse - 1, NPatchCoarse - 1)
-    # gibt den startindex fuer die obenliegende Gestalt an
-    coarseTStartIndex = util.convertpCoordIndexToLinearIndex(NPatchCoarse - 1, iSubPatchCoarse)
-
-    muTPrime = self.csi.muTPrime
-
-    numberOfElements = np.shape(coarseTIndexMap)[0]
-    NPatchFine = NPatchCoarse * NCoarseElement
-
-    ################## delta ##################
-    deltaMax = np.zeros(numberOfElements)
-    for i in range(0, numberOfElements):
-        # coarse Element Index
-        LeftCornerFineElement = util.convertpLinearIndexToCoordIndex(world.NWorldCoarse - 1,
-                                                               coarseTIndexMap[i] + coarseTStartIndex)
-        # translate in fine
-        LeftCornerFineElement *= np.min(
-            NCoarseElement)  # minimum bei eventueller coarseelement1 ungl coraseelement2
-        Patching = np.array(
-            [np.min(NCoarseElement), np.min(NCoarseElement)])  # minimum bei eventueller ungenauigkeiten
-        indexing = util.lowerLeftpIndexMap(Patching - 1, NPatchFine - 1)
-        startIndex = util.convertpCoordIndexToLinearIndex(NPatchFine - 1, LeftCornerFineElement)
-
-        deltaMax[i] = np.max(delta[indexing + startIndex])
-
-    ############## ceta #########
-    Element = elementCoarseIndex + coarseTStartIndex
-    LeftCornerFineElement = util.convertpLinearIndexToCoordIndex(world.NWorldCoarse - 1, Element)
-    LeftCornerFineElement *= np.min(NCoarseElement)  # minimum bei eventueller coarseelement1 ungl coraseelement2
-    Patching = np.array([np.min(NCoarseElement), np.min(NCoarseElement)])  # minimum bei eventueller ungenauigkeiten
-    indexing = util.lowerLeftpIndexMap(Patching - 1, NPatchFine - 1)
-    startIndex = util.convertpCoordIndexToLinearIndex(NPatchFine - 1, LeftCornerFineElement)
-
-    ########## total ###########
-    epsilonTSquare = np.sum((deltaMax ** 2) * muTPrime)
-
-    return np.sqrt(epsilonTSquare)
-
 def computeCoarseQuantitiesFlux(patch, correctorsList, aPatch):
 
     while callable(aPatch):
         aPatch = aPatch()
-    
+
     world = patch.world
     NCoarseElement = world.NCoarseElement
     NPatchCoarse = patch.NPatchCoarse
